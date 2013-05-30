3022048ac0f6ad909684424d7082d31b3d25ec6d
Winston Chang <winston@stdout.org>
2013-05-30 11:51:06 -0500

    Remove unused function

--- inst/slickgrid/shiny-slickgrid.js
+++ inst/slickgrid/shiny-slickgrid.js
@@ -1,23 +1,5 @@
 $(function(){ //DOM Ready
 
-  // Transpose a 2-dimensional array. Assumes that the array is rectangular -
-  // probably won't do the right thing if some rows are shorter.
-  function transpose(a) {
-    // Treat 'a' as a column-first array, 'b' as a row-first array
-    var cols = a.length;
-    var rows = a[0].length;
-    var b = new Array(rows);
-    var i, j;
-
-    for (i = 0; i < rows; i++) {
-      b[i] = new Array(cols);
-      for (j = 0; j < cols; j++) {
-        b[i][j] = a[j][i];
-      }
-    }
-    return b;
-  }
-
   // Convert a column-first 2D array to a row-first array of objects with
   // named entries.
   function transposeToRowObj(ar, colnames) {
